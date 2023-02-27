c0d26287cb9679350d320ba531bf026992f83b25
Laura <laura@mjalford.com>
2023-02-27 18:23:27 +0000

    Parallax Bg

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private GameObject player;
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+    public float followSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
+        float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+        gameObject.transform.position = new Vector3((x * followSpeed), y, gameObject.transform.position.z);
+    }
+
+}
